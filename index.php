6c1298d3fe4da5ad17f979700e73e65e4a8cb7ee
Alecaddd <castellani.ale@gmail.com>
2022-06-05 12:38:49 -0700

    Lesson 1

--- index.php
+++ index.php
@@ -1,11 +1,30 @@
+<?php
+$title = "This is my home page";
+$description = "This is my description";
+$x = 1;
+?>
 <html>
     <head>
         <title>PHP Tutorials</title>
     </head>
     <body>
         <h1>
-            <?php echo "THIS IS MY HOME PAGE IN PHP"; ?>
+            <?php
+            echo $title, " and", " something", " else";
+            ?>
         </h1>
-        <h2>Description</h2>
+        <h2>
+            <?php print $description; ?>
+        </h2>
+        <p>
+            <?php
+            // if ($x == 1) {
+            //     echo "YES";
+            // } else {
+            //     echo "NO";
+            // }
+            ($x == 1) ? print "YES" : print "NO";
+            ?>
+        </p>
     </body>
 </html>
